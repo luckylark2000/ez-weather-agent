dd146acd64dcd5e0b3930ab95dfecfc3c0a5acc7
luckylark2000 <3286481191@qq.com>
2025-12-24 23:19:51 +0800

    feat: add cspell configuration and environment variable

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 def main():
     print("Hello from ez-weather-agent!")
+    print(os.environ["OPEN_WEATHER_API_KEY"])
 
 
 if __name__ == "__main__":
